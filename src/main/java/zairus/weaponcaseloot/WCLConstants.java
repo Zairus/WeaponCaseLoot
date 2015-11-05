ce0f8e2b59d8d4f105ddddd6a1d98f923af9b8b3
Winter Grave <paulmongeurquijo@gmail.com>
2015-11-05 11:57:45 -0700

    Version 0.0.2 alpha
    Changed how swords are created.
    Added weapon case.
    Swords can be enchanted, this will be changed.

--- src/main/java/zairus/weaponcaseloot/WCLConstants.java
+++ src/main/java/zairus/weaponcaseloot/WCLConstants.java
@@ -4,5 +4,16 @@ public class WCLConstants
 {
 	public static final String MOD_ID = "weaponcaseloot";
 	public static final String MOD_NAME = "Weapon Case Loot";
-	public static final String MOD_VERSION = "0.0.1 alpha";
+	public static final String MOD_VERSION = "0.0.2 alpha";
+	
+	public static final String MOD_COMMON_PROXY = "zairus.weaponcaseloot.proxy.CommonProxy";
+	public static final String MOD_CLIENT_PROXY = "zairus.weaponcaseloot.proxy.ClientProxy";
+	
+	public static final String KEY_WEAPON_DURABILITY = "weapon_durability";
+	public static final String KEY_WEAPON_ATTACKDAMAGE = "weapon_damage";
+	public static final String KEY_RARITY = "weapon_rarity";
+	public static final String KEY_STATE = "weapon_state";
+	public static final String KEY_WEAPONINDEX = "weapon_index";
+	
+	public static final int totalSwords = 5;
 }
--- src/main/java/zairus/weaponcaseloot/WeaponCaseLoot.java
+++ src/main/java/zairus/weaponcaseloot/WeaponCaseLoot.java
@@ -4,14 +4,19 @@ import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import zairus.weaponcaseloot.proxy.CommonProxy;
 
 @Mod(modid = WCLConstants.MOD_ID, name = WCLConstants.MOD_NAME, version = WCLConstants.MOD_VERSION)
 public class WeaponCaseLoot
 {
 	public static Logger logger;
+	
+	@SidedProxy(clientSide = WCLConstants.MOD_CLIENT_PROXY, serverSide = WCLConstants.MOD_COMMON_PROXY)
+	public static CommonProxy proxy;
 	
 	@Mod.Instance(WCLConstants.MOD_ID)
 	public static WeaponCaseLoot instance;
@@ -20,17 +25,21 @@ public class WeaponCaseLoot
 	public void preInit(FMLPreInitializationEvent event)
 	{
 		logger = event.getModLog();
+		
+		WeaponCaseLoot.proxy.preInit(event);
+		
+		WCLConfig.init(event.getSuggestedConfigurationFile());
 	}
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
-		;
+		WeaponCaseLoot.proxy.init(event);
 	}
 	
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
-		;
+		WeaponCaseLoot.proxy.postInit(event);
 	}
 }
